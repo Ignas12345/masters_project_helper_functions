d6adfebf8578a688e4e478fc3d00915e566b3ec8
Ignas12345 <155150364+Ignas12345@users.noreply.github.com>
2025-07-02 08:23:29 +0000

    bug fix for writing results

--- classification_pipeline.py
+++ classification_pipeline.py
@@ -126,8 +126,7 @@ def run_pipeline_across_folds(df, sample_label_dict, train_folds_df, test_folds_
     results_for_single_fold = run_pipeline_for_single_fold(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
                                                             pre_processing_methods=pre_processing_methods, feature_selection_method=feature_selection_method,
                                                             classification_model=classification_model, **kwargs)
-    results_for_single_fold = pd.DataFrame(results_for_single_fold)
-    results_df.loc[fold_index] = results_for_single_fold
+    results_df.loc[fold_index] = pd.Series(results_for_single_fold)
 
     if autosave:
       results_df.to_csv(save_path + 'results_df_autosave.csv', sep = ';')
